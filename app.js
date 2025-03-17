415d59ffc691d4ed57df05a26bbe92bcfed83139
NatiSoriaa <naticsoria@gmail.com>
2025-03-17 23:11:54 +0100

    M8

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const products_routes = require('./routes/products.js')
+const slugify = require('slugify');
+
 
 //Server instantiation
 const app = express()
@@ -8,6 +10,13 @@ const app = express()
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('/views'));
+
+app.get('/api', (req, res) => {
+    const message = 'Bienvenido a nuestra API';
+    const slugMessage = slugify(message, { replacement: '*' });
+    res.send(`<h1>${slugMessage}</h1>`);
+});
+
 
 //Midleware
 app.use(express.json())
